1a21dbfea055007bd36d6f79b0e38303021e4694
Devanshi Mallick <98609944+DevanshiMallick@users.noreply.github.com>
2024-08-27 17:00:38 +0530

    Add files via upload

---
+++ CPP/29. Print diamond pattern.cpp
@@ -0,0 +1,36 @@
+/*Print diamond shape*/
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        int stars=i*2-1;
+        int spaces= n-i;
+        for (int j=1; j<=spaces;j++){
+            cout<<" ";
+        }
+        
+        for (int j=1; j<=stars;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for (int i=1; i<=n-1;i++){
+        int stars=2*(n-i)-1;
+        int spaces= i;
+        for (int j=1; j<=spaces;j++){
+            cout<<" ";
+        }
+        
+        for (int j=1; j<=stars; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
